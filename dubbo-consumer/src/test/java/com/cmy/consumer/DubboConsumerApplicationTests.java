b5227832b124232ab3051a0e8890f61b535b1c74
chenmy <Wpt19930130>
2020-04-11 21:43:42 +0800

    t

---
+++ dubbo-consumer/src/test/java/com/cmy/consumer/DubboConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmy.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
